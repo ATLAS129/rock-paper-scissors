e43be9cc914f33afe10a6fa3df81f8df7da3656b
ATLAS129 <yanis20092525@gmail.com>
2024-11-20 22:33:47 +0200

    added more logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,98 @@
 import { useState } from "react";
 import { FaHandPaper, FaHandRock, FaHandScissors } from "react-icons/fa";
 
-const actions = [
-  <FaHandRock size={100} />,
-  <FaHandPaper size={100} />,
-  <FaHandScissors size={100} />,
-];
-// const aiActions = [
-//   <FaHandRock size={100} />,
-//   <FaHandPaper size={100} />,
-//   <FaHandScissors size={100} />,
-// ];
+const actions = {
+  rock: <FaHandRock size={100} />,
+  paper: <FaHandPaper size={100} />,
+  scissors: <FaHandScissors size={100} />,
+};
 export default function App() {
   const [result, setResult] = useState("");
   const [action, setAction] = useState();
   const [aiAction, setAiAction] = useState();
-  // const aiAction = Math.floor(Math.random() * 2);
-  // const  = aiActions[aiRandom];
-  // if (action) {
-  //   const results = {
-  //     rock: "scissors",
-  //     paper: "rock",
-  //     scissors: "paper",
-  //   };
-
-  //   setResult(
-  //     results[action] == aiAction
-  //       ? "Win"
-  //       : action === aiAction
-  //       ? "Draw"
-  //       : "Loss"
-  //   );
-  // }
 
   function handleGame(action) {
+    const results = {
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
+    };
+    const aiRandom = ["rock", "paper", "scissors"][
+      Math.floor(Math.random() * 3)
+    ];
     setAction(action);
-    setAiAction(Math.floor(Math.random() * 3));
+    setAiAction(aiRandom);
+    if (results[action] === aiRandom) {
+      setResult("Win");
+    } else if (action === aiRandom) {
+      setResult("Tie");
+    } else {
+      setResult("Loss");
+    }
   }
 
   console.log([action, aiAction]);
 
   return (
-    <div className="flex justify-center items-center gap-3 flex-col text-center p-2">
-      <h1 className="text-3xl font-semibold">Rock Paper Scissors</h1>
-      <div>
-        <h2 className="text-3xl p-3">Result: {result}</h2>
-        <div className="flex gap-10 text-3xl items-center">
-          You {actions[action]} - {actions[aiAction]} AI
+    <div className="w-full h-screen flex flex-col justify-between items-center p-2">
+      <h1 className="text-3xl">Rock Paper Scissors</h1>
+      <div className="flex justify-center items-center gap-3 flex-col text-center">
+        {action && aiAction && result && (
+          <div>
+            <h2 className="text-3xl p-3">
+              Result:{" "}
+              <span
+                className={
+                  "text-4xl " +
+                  (result === "Win"
+                    ? "text-green-500"
+                    : result === "Tie"
+                    ? "text-gray-400"
+                    : result === "Loss"
+                    ? "text-red-500"
+                    : "")
+                }
+              >
+                {result}
+              </span>
+            </h2>
+            <div className="flex gap-10 text-3xl items-center">
+              You {actions[action]} - {actions[aiAction]} AI
+            </div>
+          </div>
+        )}
+        <div className="w-full flex justify-center items-center gap-3 flex-col">
+          <h2 className="text-xl">Choose your action:</h2>
+          <div className="flex gap-5 p-2">
+            <FaHandRock
+              size={100}
+              className="cursor-pointer"
+              onClick={() => handleGame("rock")}
+            />
+            <FaHandPaper
+              size={100}
+              className="cursor-pointer"
+              onClick={() => handleGame("paper")}
+            />
+            <FaHandScissors
+              size={100}
+              className="cursor-pointer"
+              onClick={() => handleGame("scissors")}
+            />
+          </div>
+          <button
+            className="border py-2 px-4 bg-white text-black rounded-lg"
+            onClick={() => {
+              setResult("");
+              setAction("");
+              setAiAction("");
+            }}
+          >
+            Reset
+          </button>
         </div>
       </div>
-      <div className="border-t w-full flex justify-center items-center flex-col">
-        <h2 className="text-xl">Choose your action:</h2>
-        <div className="flex gap-5 p-2">
-          <FaHandRock
-            size={100}
-            className="cursor-pointer"
-            onClick={() => handleGame(0)}
-          />
-          <FaHandPaper
-            size={100}
-            className="cursor-pointer"
-            onClick={() => handleGame(1)}
-          />
-          <FaHandScissors
-            size={100}
-            className="cursor-pointer"
-            onClick={() => handleGame(2)}
-          />
-        </div>
-        {/* <button className="border py-2 px-4 bg-white text-black rounded-lg">
-          Reset
-        </button> */}
-      </div>
+      <h1 className="text-3xl">Thanks for checking out💖</h1>
     </div>
   );
 }
